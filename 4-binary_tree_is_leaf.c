c5400cb509e0616f080a7a3dbdfcb173851e3c32
Olumadeopeyemi <dimejiolus@gmail.com>
2023-03-02 14:25:06 -0800

    commit for 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,4 @@
-#include "binary_trees.h"
+#include "binary_trees.h"
 /**
  * binary_tree_is_leaf - know if a node is a leaf, that means that the node
  * does not have any child neither left nor right
@@ -12,3 +12,4 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
+}
