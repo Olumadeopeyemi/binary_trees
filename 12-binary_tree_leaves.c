49c9ef613da7a436182935a8514ad839105a5bd4
lolaibikz <ibikunle.oluwafunmilola@gmail.com>
2023-03-02 13:29:41 -0800

    corrected tasks

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,18 +1,21 @@
-include "binary_trees.h"
-
+#include "binary_trees.h"
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: is a pointer to the root node of the tree to count the number of leaves
- *
+ * binary_tree_leaves - function that returns the number of leaves in a tree
+ * @tree: tree to check
  * Return: number of leaves
- *         0 if tree is NULL
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (!tree)
+	size_t leaf = 0, l = 0, r = 0;
+
+	if (tree == NULL)
+	{
 		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+	else
+	{
+		l = binary_tree_leaves(tree->left);
+		r = binary_tree_leaves(tree->right);
+		leaf = l + r;
+		return ((!l && !r) ? leaf + 1 : leaf + 0);
+	}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
- * @tree: is a pointer to the root node of the tree to measure the height
- *
- * Return: height of the tree
- *         0 if tree is NULL
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	size_t l = 0;
+	size_t r = 0;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
